3306dd1a52fd7431791ad5fca8e1178eec8a1b70
yorifuji <yorifuji@gmail.com>
2024-12-04 19:18:01 +0900

    Use Person class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:freezed_code_reading/person/person.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,12 +8,14 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  final person = const Person(firstName: 'John', lastName: 'Doe', age: 30);
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Hello World! ${person.firstName} ${person.lastName}'),
         ),
       ),
     );
